8bf77b15ad08c2329542bf69e91246edfbed897c
PaciniAn <PaciniAn@vt-paciniand.valueteam.com>
2020-04-24 11:47:00 +0200

    test

--- demo-docker/src/main/java/com/example/demodocker/DemoController.java
+++ demo-docker/src/main/java/com/example/demodocker/DemoController.java
@@ -16,6 +16,7 @@ public class DemoController
         List<Customer> customerList = new ArrayList<Customer>();
         customerList.add(new Customer(1, "FORZA ROMA"));
         customerList.add(new Customer(2, "CIAO PIPPO"));
+        customerList.add(new Customer(3, "MODIFICA FATTA SUL BRANCH"));
         return customerList;
     }
 }
